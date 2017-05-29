434029db90e50dc042f037e4ec3027ea61bc1948
Felipe Ortega Casillas <felipeorcas@gmail.com>
2017-05-28 20:14:33 -0700

    more changes

--- app/includes/session.php
+++ app/includes/session.php
@@ -1,16 +1,23 @@
 <?php
-   include('db_connect.php');
-   session_start();
+    include('db_connect.php');
+    session_start();
    
-   $user_check = $_SESSION['Email'];
-   
-   $ses_sql = mysqli_query($mysqli,"SELECT Email FROM account WHERE Email = '$user_check' ");
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
-   $login_session = $row['Email'];
-   
-   if(!isset($_SESSION['Email'])){
-      header("location:../login_user/login_user.views.php");
+    if(!empty($_SESSION['Email'])){
+
+        $user_check = $_SESSION['Email'];
+        
+        $ses_sql = mysqli_query($mysqli,"SELECT Email FROM account WHERE Email = '$user_check' ");
+        
+        $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+        
+        $login_session = $row['Email'];
+        
+        if(!isset($_SESSION['Email'])){
+
+            header("location:../login_user/login_user.views.php");
+        }
+   }else{
+       session_destroy();
+       header("location: ../login_user/login_user.views.php");
    }
 ?>
--- app/includes/user_login.php
+++ app/includes/user_login.php
@@ -2,16 +2,19 @@
 include("db_connect.php");
 session_start();
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-      
+if(!empty($_SESSION['Email'])) {
+      header("location: ../user_views/main.php");
+      exit;
+}
+
+ if($_POST && !empty($_POST['Email'] && !empty($_POST['Password'])) {     
       // username and password sent from form 
       $myemail = mysqli_real_escape_string($mysqli,$_POST['Email']);
       $mypassword = mysqli_real_escape_string($mysqli,$_POST['Password']); 
       
-      $sql = "SELECT Id FROM accounts WHERE Email = '$myemail' and Password = '$mypassword'";
+      $sql = "SELECT Id FROM accounts WHERE Email = '$myemail' AND Password = '$mypassword'";
       $result = mysqli_query($mysqli,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
@@ -21,6 +24,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
          
          header("location: ../user_views/main.php");
       }else {
+            session_destroy();
          $error = "Your Login Name or Password is invalid";
          //Agregar pagina que no estas registrado
          header("location: ../login_user/login_user.views.php");
