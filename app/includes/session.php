7432222ad89d670ed5d93ea065321d3b5512994f
Felipe Ortega Casillas <felipeorcas@gmail.com>
2017-05-28 21:31:53 -0700

    changes

--- app/includes/session.php
+++ app/includes/session.php
@@ -6,12 +6,13 @@
 
         $user_check = $_SESSION['id_user'];
         
-        $ses_sql = mysqli_query($mysqli,"SELECT id_U FROM User WHERE id_U = '$user_check' ");
+        $ses_sql = mysqli_query($mysqli,"SELECT id_U, name_U FROM User WHERE id_U = '$user_check' ");
         
         $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
         
         $login_session = $row['id_U'];
         
+        $login_name = $row['name_U'];
         if(!isset($_SESSION['id_user'])){
 
             header("location:../login_user/login_user.views.php");
