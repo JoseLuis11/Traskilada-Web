22d9f997002bee4680e26998987794ccdd2e9656
JoseLuis <jluis.scooter11@hotmail.com>
2017-05-30 05:03:14 -0700

    Este tambien alv

--- app/includes/list_employees.php
+++ app/includes/list_employees.php
@@ -11,6 +11,7 @@
     
     while($row=$result->fetch_array(MYSQLI_ASSOC)){
         ?>
+        <option value="<?php echo $row["name"] //. echo $row['lastName'];?>"><?php echo $row["name"] //. echo $row['lastName'];?></option>
 <?php     
     }   
     mysqli_close($mysqli);  
