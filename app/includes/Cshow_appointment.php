d1deb73e3ad35372d4907f60b9d4a935b6518a6e
José Luis Castro <jluis.scooter11@hotmail.com>
2017-12-06 15:36:31 -0800

    last changes six months ago

--- app/includes/Cshow_appointment.php
+++ app/includes/Cshow_appointment.php
@@ -4,13 +4,17 @@ include("../includes/db_connect.php");
 //show user appointment query
 $result = $mysqli->query("SELECT DATE_FORMAT(a.dateTime,'%b %d %Y, %h:%i %p') as 'Fecha y hora', w.name_W as Local, CONCAT(e.name_E, ' ', e.lastName_E) as Empleado, s.name_S as Servicios
 FROM workplace w, user u, employee e, service s, Service_Appointment sa, appointment a, owner o, Employee_Workplace_Owner ewo
-WHERE s.id_S = sa.id_S
-AND sa.id_A = a.id_A 
-AND a.id_U = u.id_U
-AND o.id_O = ewo.id_O
-AND e.id_E = ewo.id_E
+WHERE w.id_W = a.id_W
+AND w.id_O = o.id_O
+AND u.id_U = a.id_U
+AND w.id_W = ewo.id_W
+AND ewo.id_O = o.id_O
+AND a.id_E = e.id_E
+AND a.id_E = ewo.id_E
+AND a.id_W = ewo.id_W
+AND sa.id_A = a.id_A
+AND s.id_S = sa.id_S
 AND u.id_U = $login_id
-AND ewo.id_W = w.id_W
 group by a.dateTime
-order by a.dateTime ASC");
+order by a.dateTime ASC;");
 ?>
--- app/index/index2.views.php
+++ app/index/index2.views.php
@@ -9,8 +9,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="../assets/css/index2.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+<link rel="stylesheet" href="../assets/css/index2.css">
 <link rel="icon" href="../assets/img/favicon.ico">
 <style>
 body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
